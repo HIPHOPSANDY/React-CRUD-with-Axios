95a70cdbec4699fc8cd758147f473bdcabc7172d
HIPHOPSANDY <santhoshkumar24cs@gamil.com>
2021-07-16 22:32:17 +0530

    first commit

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import EmployeeService from '../Service/EmployeeService';
+
+class ListEmployeeComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            emloyees:[]    
+        }
+    }
+
+
+    componentDidMount() {
+        EmployeeService.getEmployees().then((res) => {
+            this.setState({emloyees: res.data})
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2 className="text-center">Employee List</h2>
+                <div className="row">
+                
+               
+                <table className="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th scope="col">Employee First Name</th>
+                      <th scope="col">Employee Last Name</th>
+                      <th scope="col">Employee Email Id</th>
+                      <th scope="col">Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      {
+                          this.state.emloyees.map(
+                              employee => 
+                              <tr key={employee.id}>
+                                  <td>
+                                      {employee.firstName}
+                                  </td>
+                                  <td>
+                                      {employee.lastName}
+                                  </td>
+                                  <td>
+                                      {employee.emailId}
+                                  </td>
+                              </tr>
+                          )
+                            
+                      }
+                    
+                  </tbody>
+                </table>
+                </div>
+                
+                </div>
+                
+            
+        );
+    }
+}
+
+export default ListEmployeeComponent;
